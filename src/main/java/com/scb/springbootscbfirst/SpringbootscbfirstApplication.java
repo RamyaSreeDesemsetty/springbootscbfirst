29c79a05493a25eb0bec98fe711b196a6fa4a750
RamyaSreeDesemsetty <ramyadesemsetty@gmail.com>
2024-11-13 22:48:06 +0530

    changes made

---
+++ src/main/java/com/scb/springbootscbfirst/SpringbootscbfirstApplication.java
@@ -0,0 +1,13 @@
+package com.scb.springbootscbfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootscbfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootscbfirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scb/springbootscbfirst/SpringbootscbfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scb.springbootscbfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootscbfirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
